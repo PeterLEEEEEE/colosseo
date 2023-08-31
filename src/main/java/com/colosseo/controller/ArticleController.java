a8d71cd07cb791155acf546e8a6d8da7bf2f8a53
imuhyeon <dissgogo@naver.com>
2023-08-31 10:28:35 +0900

    swagger explained

--- src/main/java/com/colosseo/controller/ArticleController.java
+++ src/main/java/com/colosseo/controller/ArticleController.java
@@ -3,7 +3,9 @@ package com.colosseo.controller;
 import com.colosseo.dto.article.ArticleRequestDto;
 import com.colosseo.global.config.security.UserPrincipal;
 import com.colosseo.service.article.ArticleService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
@@ -16,9 +18,12 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @SecurityRequirement(name = "jwtAuth")
 @RequestMapping("/api/v1/articles")
+@Tag(name = "게시글 API", description = "게시글 CRUD API")
 public class ArticleController {
 
     private final ArticleService articleService;
+
+    @Operation(summary = "특정 게시글과 댓글 가져오기", description = "유저 로그인 API")
     @GetMapping("/{articleId}")
     public String getPostWithComments(@PathVariable Long articleId) {
         return "test";
--- src/main/java/com/colosseo/controller/UserController.java
+++ src/main/java/com/colosseo/controller/UserController.java
@@ -5,10 +5,10 @@ import com.colosseo.dto.user.TokenRequestDto;
 import com.colosseo.dto.user.TokenResponseDto;
 import com.colosseo.dto.user.UserLoginRequestDto;
 import com.colosseo.dto.user.UserSignupRequestDto;
-import com.colosseo.service.email.EmailService;
 import com.colosseo.service.email.EmailServiceImpl;
 import com.colosseo.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 @Slf4j
+@Tag(name = "유저 API", description = "유저 인증 관련 API")
 public class UserController {
     private final UserService userService;
     private final EmailServiceImpl emailService;
@@ -41,11 +42,13 @@ public class UserController {
         return ResponseEntity.ok().body(userService.logout(tokenDto));
     }
 
+    @Operation(summary = "이메일 인증코드 요청 API", description = "유저 회원가입 시 이메일로 인증코드 요청하는 API")
     @PostMapping("/email-confirm")
     public ResponseEntity<String> emailConfirm(@RequestParam String email) throws Exception {
         return ResponseEntity.ok().body(emailService.sendMessage(email));
     }
 
+    @Operation(summary = "이메일 인증코드 확인 API", description = "회원가입 시 이메일 인증코드 확인 API")
     @PostMapping("/email-verification")
     public ResponseEntity<Boolean> emailCode(@Valid @RequestBody CodeRequestDto codeRequestDto) {
         return ResponseEntity.ok().body(emailService.verifyCode(codeRequestDto));
