cd809414a599359be2ae5fb0f034721b5445a0ed
imuhyeon <dissgogo@naver.com>
2023-09-01 17:22:26 +0900

    sh file setup

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+
+time=$(date)
+
+echo "Building App..."
+cd /home/nunaps/dev/springman/colosseo
+git pull origin main
+./gradlew clean build
+
+echo "Stopping Containers..."
+echo "Removing Images..."
+docker-compose down --rmi all
+
+echo "Setup New Containers..."
+docker-compose -f dokcer-compose.yml up -d
+
+echo "Complete!!!"
