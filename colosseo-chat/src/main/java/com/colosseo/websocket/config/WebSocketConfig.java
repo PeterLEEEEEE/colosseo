5b495bfa3423e28bad55515a3b4f2513b0efa5da
사용자1 <sayongja1@sayongja1ui-iMac.local>
2024-02-08 20:28:39 +0900

    multi module setup

---
+++ colosseo-chat/src/main/java/com/colosseo/websocket/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.colosseo.websocket.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/ws");
+    }
+
+}
